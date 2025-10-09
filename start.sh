3cdd8c098de72d31f2d400caf5247145b3975662
Zaynab Hmede <105450135+zahweb@users.noreply.github.com>
2025-10-09 13:55:51 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:$PORT app:app --workers 2 --threads 4 --timeout 60
+echo "🚀 Starting server with Gunicorn..."
+gunicorn --bind 0.0.0.0:$PORT app:app --timeout 120 --workers 1
